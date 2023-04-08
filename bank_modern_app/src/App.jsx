d9d114f96fc09a01337fbf6aeb75d96bad5c2bdb
acc1iela <acc1iela@gmail.com>
2023-04-09 03:47:57 +0900

    update: App.jsxでコンポーネント呼び込み

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -9,12 +9,22 @@ import {
   Footer,
   Stats,
   Testimonials,
+  Hero,
+  Navbar,
 } from './components';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
